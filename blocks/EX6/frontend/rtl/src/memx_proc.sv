485dfa2cd72214d10bdbbb47d487f8a9be83a473
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-09-24 19:46:09 -0300

    Criação de memx_proc

---
+++ blocks/EX6/frontend/rtl/src/memx_proc.sv
@@ -0,0 +1,23 @@
+module memx_proc
+  (
+    // Interface de entrada e saídas externas
+    input  logic                      clk_i,
+    input  logic                      memc_wd_i,
+    input  logic [RAM_ADDR_WIDTH-1:0] memc_addr_i,
+    input  logic [RAM_DATA_WIDTH-1:0] memc_wdt_i,
+    output logic [RAM_DATA_WIDTH-1:0] memc_rdt_o,
+    // Interface com o modelo de memória
+    input  logic [RAM_DATA_WIDTH-1:0] data_i,
+    output logic [RAM_ADDR_WIDTH-1:0] addr_o,
+    output logic [RAM_DATA_WIDTH-1:0] data_o
+  );
+
+  always_ff @(posedge clk_i)
+  begin
+    addr_o <= memc_addr_i;
+    data_o <= memc_wdt_i;
+    wr_i <= memc_wd_i;
+    memc_rdt_o <= data_i;
+  end
+
+endmodule
