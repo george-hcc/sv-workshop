4afa7bf0a64203007c722661a07861e12b6a8683
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-10-02 22:42:48 -0300

    Ajustes no testbench do exercicio 6

--- blocks/EX6/frontend/rtl/tb/tb.sv
+++ blocks/EX6/frontend/rtl/tb/tb.sv
@@ -6,6 +6,8 @@ module tb();
   parameter RAM_N_OF_WORDS = 256;
   
   logic                       clk;
+  logic [1:0]                 clk_div;
+  logic                       rstn;
   //Interface TB-MEMC
   logic                       mem_rd_w;
   logic                       mem_wr_w;
@@ -26,6 +28,8 @@ module tb();
   //Estatisticas de teste
   int n_erros;
   int n_testes;
+
+  always_ff @(posedge clk) clk_div += 1;
   
   memx
   #(
@@ -34,6 +38,7 @@ module tb();
     .RAM_N_OF_WORDS()
   )CONTROLLER(
     .clk_i              (clk),
+    .rst_n_i            (rstn),
     .mem_rd_i           (mem_rd_w),
     .mem_wr_i           (mem_wr_w),
     .mem_addr_i         (mem_addr_w),
@@ -41,24 +46,22 @@ module tb();
     .mem_busy_o         (mem_busy_w),
     .mem_rdt_o          (mem_rdt_w),
     .mem_wok_o          (mem_wok_w),
-    .busy_mem_i         (busy_mem_w),
     .data_i             (rdata_w),
     .wr_o               (wr_w),
     .addr_o             (addr_w),
     .data_o             (wdata_w)
   );
 
-  mem_fpga
+  mem_model
   #(
     .RAM_DATA_WIDTH(),
     .RAM_ADDR_WIDTH(),
     .RAM_N_OF_WORDS()
   )MEMORY(
-    .clk_i               (clk),
+    .clk_i               (clk_div[1]),
     .wr_i                (wr_w),
     .addr_i              (addr_w),
     .data_i              (wdata_w),
-    .busy_mem_o          (busy_mem_w),
     .data_o              (rdata_w)
   );
   
@@ -68,17 +71,19 @@ module tb();
     mem_rd_w = 1'b0;
     mem_addr_w = 10'h000;
     mem_wdt_w = 16'h1010;
-    toggle_clk(40000);
-    /*
-    $display("###INICIALIZANDO TESTES###");
-    for(int i = 0; i < 1000; i++) begin
-      test_sad(i);
-      n_testes++;
-    end
-    $display("######FIM DE TESTES$######");
-    $display("Número de Testes:\t%d", n_testes);
-    $display("Número de Erros:\t%d", n_erros);
-    */
+    toggle_clk(10);
+    while(mem_busy_w)
+      toggle_clk(1);
+    mem_addr_w++;
+    mem_wdt_w = mem_wdt_w * 3;
+    toggle_clk(10);
+    while(mem_busy_w)
+      toggle_clk(1);
+    mem_addr_w++;
+    mem_wdt_w = mem_wdt_w * 3;
+    toggle_clk(10);
+    while(mem_busy_w)
+      toggle_clk(1);
     $finish;
   end
   
@@ -86,49 +91,12 @@ module tb();
     n_erros = 0;
     n_testes = 0;
     clk = 1'b0;
-    toggle_clk(1);
-  endtask
-
-/*
-  task test_sad(int seed);
-    int pixel_index;
-    int pixel_array_a [0:255];
-    int pixel_array_b [0:255];
-
-    $srandom(seed);
-    for(int i = 0; i < 256; i++)
-    begin
-      pixel_array_a[i] = $urandom_range(0, 255);
-      pixel_array_b[i] = $urandom_range(0, 255);
-    end
-
-    pixel_index = 0;
-    while(pixel_index < 255)
-    begin
-      data_a = pixel_array_a[pixel_index];
-      data_b = pixel_array_b[pixel_index];
-      while(!busy)
-        toggle_clk(1);
-      toggle_clk(1);
-      pixel_index++;
-    end
-
+    clk_div = 2'h0;
+    rstn = 1'b0;
     toggle_clk(2);
-    check_error(pixel_array_a, pixel_array_b);
+    rstn = 1'b1;
+    toggle_clk(2);
   endtask
-
-  task check_error(int pa_a [0:255], int pa_b [0:255]);
-    int reff;
-    reff = 0;
-    for(int i = 0; i < 255; i++)
-      reff = reff + $abs(pa_a[i] - pa_b[i]);
-    if(reff != data_out)
-    begin
-      $display("#ERRO: data_out = %d, reff = %f", data_out, reff);
-      n_erros++;
-    end
-  endtask
-*/
   
   task toggle_clk(input int n);
     for(int i = 0; i < n; i++) 
