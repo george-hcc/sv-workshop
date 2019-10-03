15db4a2564dde1a1bfede846ee40c99b0b7af275
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-10-02 22:20:07 -0300

    Remodelagem e simplificação de memória assíncrona do exercicio 6

--- blocks/EX6/frontend/rtl/src/mem_model.sv
+++ blocks/EX6/frontend/rtl/src/mem_model.sv
@@ -1,4 +1,4 @@
-module mem_fpga
+module mem_model
   #(
     parameter RAM_DATA_WIDTH = 16,
     parameter RAM_ADDR_WIDTH = 10,
@@ -8,66 +8,19 @@ module mem_fpga
     input  logic                       wr_i,
     input  logic [RAM_ADDR_WIDTH-1:0]  addr_i,
     input  logic [RAM_DATA_WIDTH-1:0]  data_i,
-    output logic                       busy_mem_o,
     output logic [RAM_DATA_WIDTH-1:0]  data_o
   );
 
-  // Referencia de contagem de worst case considerando clock de 500kHz
-  localparam WORST_CASE_RD_CYCLES = 50;
-  localparam WORST_CASE_WR_CYCLES = 7500;
-
-  // Memoria
-  logic [RAM_DATA_WIDTH-1:0]  data_vector [0:RAM_N_OF_WORDS-1];
-  logic [RAM_ADDR_WIDTH-1:0]  addr_r;
-  logic [12:0]                counter;
-
-  // Criação de variável para substituir o delay de escrita que não é possivel de se fazer de forma sequencial
-  logic                       gambiarra;
-  
-  enum
-  {
-    IDLE,
-    READ_PROC,
-    WRITE_PROC
-  }state, next_state;
-
-  //initial $readmemh("endereço do folder", data_vector);
-
-  always_comb
-  begin
-    unique case(state)
-      IDLE:       
-        next_state = (wr_i) ? (WRITE_PROC) : (READ_PROC);
-      READ_PROC:  
-        next_state = (counter == WORST_CASE_RD_CYCLES) ? (IDLE) : (READ_PROC);
-      WRITE_PROC:
-        next_state = (counter == WORST_CASE_WR_CYCLES) ? (IDLE) : (WRITE_PROC);
-    endcase
-  end
-
-  assign #(5ms:10ms:15ms) gambiarra = (wr_i) ? (1'b1) : (1'b0);
+  logic [RAM_DATA_WIDTH-1:0] data_vector [RAM_N_OF_WORDS];
+  logic [RAM_ADDR_WIDTH-1:0] addr_r;
 
   always_ff @(posedge clk_i)
   begin
-    state <= next_state;
     addr_r <= addr_i;
-    if(gambiarra) 
-      data_vector[addr_r] <= data_i;
-    if(state == IDLE)
-      counter <= 13'h0000;
-    else
-      counter <= counter + 1'b1;
+    data_o <= #(20us:60us:100us) data_vector[addr_r];
+    if(wr_i) 
+      data_vector[addr_r] <= #(5ms:10ms:15ms) data_i;
   end
-
-  always_ff @(negedge clk_i)
-  begin
-    if(state == IDLE)
-      busy_mem_o = 1'b0;
-    else
-      busy_mem_o = 1'b1;
-  end
-  
-  assign #(20us:60us:100us) data_o = data_vector[addr_r];
 
 endmodule
 
