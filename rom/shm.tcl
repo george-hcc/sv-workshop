e2e41fe932652c70cef9fccf9e8ccfb31080c2ec
George Camboim <george.camboim@embedded.ufcg.edu.br>
2021-03-04 16:36:09 -0300

    Renaming folders and creating a new unified structure to the repo

--- rom/shm.tcl
+++
@@ -1,7 +0,0 @@
-database -open waves -shm
-probe -create testbench -depth all -all -memories -shm -database waves -name proba
-probe -enable proba
-run 18 ms  -absolute
-#probe -disable proba
-#run 70 ms -absolute
-exit
